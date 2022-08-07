9247e43efc8c68362a885f519897e9fea803c312
Simrit-Momenttext <simrit.kaul@momenttext.com>
2022-08-07 15:32:01 +0530

    Tried edge smoothening

--- transparent.py
+++ transparent.py
@@ -1,9 +1,34 @@
 from PIL import Image
 import os
+import cv2
+import numpy as np
+
+def contour_smooth(imgPath, savePath):
+
+  img = cv2.imread(imgPath)
+
+  blurred_img = cv2.GaussianBlur(img, (21, 21), 0)
+  mask = np.zeros(img.shape, np.uint8)
+
+  gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+  ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+  contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+  cv2.drawContours(mask, contours, -1, (255,255,255),5)
+  output = np.where(mask==np.array([255, 255, 255]), blurred_img, img)
+
+  print('Edge Smoothening Done')
+
+  os.remove(imgPath)
+  
+  cv2.imwrite(savePath, output)
+
 
 '''Transparent BG Image'''
 def convertImageBG(imagePath, savePath):
+
   img = Image.open(imagePath)
+
   img = img.convert("RGBA")
   
   data = img.getdata()
@@ -12,7 +37,27 @@ def convertImageBG(imagePath, savePath):
   newData = []
   
   for item in data:
-      if (item[0] <= 20 and item[1] >= 230 and item[2] <= 20 and item[3] >= 240):
+      if (item[0] <= 80 and item[1] >= 180 and item[2] <= 100 and item[3] >= 250):
+          newData.append((255, 255, 255, 0))
+      else:
+          newData.append(item)
+  
+  img.putdata(newData)
+  img.save(savePath, "PNG")
+
+  contour_smooth(savePath, f'remove_bg\smooth-edge.png')
+
+  img = Image.open(f'remove_bg\smooth-edge.png')
+
+  img = img.convert("RGBA")
+  
+  data = img.getdata()
+#   print(list(data))
+
+  newData = []
+  
+  for item in data:
+      if (item[0] >= 250 and item[1] >= 250 and item[2] >= 250 and item[3] >= 250):
           newData.append((255, 255, 255, 0))
       else:
           newData.append(item)
@@ -21,4 +66,4 @@ def convertImageBG(imagePath, savePath):
   img.save(savePath, "PNG")
   print("Transparent Segment Background Done")
 
-  os.remove(imagePath)
+  os.remove(f'remove_bg\smooth-edge.png')
